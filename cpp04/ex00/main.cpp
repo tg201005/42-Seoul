1c0acac98c0f59c986fd27437624e3d68d4eca81
tg201005 <teemo20001005@gmail.com>
2023-06-06 18:08:41 +0900

    add main

--- cpp04/ex00/main.cpp
+++ cpp04/ex00/main.cpp
@@ -1,32 +1,38 @@
 #include "Animal.hpp"
 #include "WrongAnimal.hpp"
 
+void checkLeaks()
+{
+    system("leaks a.out");
+}
+
 int main()
 {
-const Animal* meta = new Animal();
-const Animal* j = new Dog();
-const Animal* i = new Cat();
-std::cout << j->getType() << " " << std::endl;
-std::cout << i->getType() << " " << std::endl;
-i->makeSound(); //will output the cat sound!
-j->makeSound();
-meta->makeSound();
+    const Animal* meta = new Animal();
+    const Animal* j = new Dog();
+    const Animal* i = new Cat();
+    std::cout << j->getType() << " " << std::endl;
+    std::cout << i->getType() << " " << std::endl;
+    i->makeSound(); //will output the cat sound!
+    j->makeSound();
+    meta->makeSound();
 
-delete meta;
-delete j;
-delete i;
+    delete meta;
+    delete j;
+    delete i;
 
-std::cout << "---------------------" << std::endl;
-const WrongAnimal* k = new WrongAnimal();
-const WrongAnimal* q = new WrongCat();
-std::cout << k->getType() << " " << std::endl;
-std::cout << q->getType() << " " << std::endl;
+    std::cout << "---------------------" << std::endl;
+    const WrongAnimal* k = new WrongAnimal();
+    const WrongAnimal* q = new WrongCat();
+    std::cout << k->getType() << " " << std::endl;
+    std::cout << q->getType() << " " << std::endl;
 
-q->makeSound();
-k->makeSound();
+    q->makeSound();
+    k->makeSound();
 
-delete k;
-delete q;
+    delete k;
+    delete q;
 
-return 0;
+    atexit(checkLeaks); 
+    return 0;
 }
