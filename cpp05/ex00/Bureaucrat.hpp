48483555732107fb1d03e3caee4f4ba8c7bedba3
tg201005 <teemo20001005@gmail.com>
2023-06-07 14:39:32 +0900

    dfd

---
+++ cpp05/ex00/Bureaucrat.hpp
@@ -0,0 +1,17 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+
+#include <iostream>
+#include <string>
+#include <exception>
+
+class Bureaucrat
+{
+    private:
+        const std::string	_name;
+        int					_grade;
+        Bureaucrat();
+        
+    public:
+};
