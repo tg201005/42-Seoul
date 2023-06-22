cb5208944557661402b8c298395e6d9208c33470
Taekwon Yi <tyi@c8r2s5.42seoul.kr>
2023-06-22 21:26:14 +0900

    first``

--- cpp05/ex02/Form.hpp
+++ cpp05/ex02/Form.hpp
@@ -1,35 +1,33 @@
-#ifndef AForm_HPP
-#define AForm_HPP
+#ifndef FORM_HPP
+#define FORM_HPP
 
 #include <iostream>
 #include <string>
 #include <exception>
 #include "Bureaucrat.hpp"
 
-
 class Bureaucrat;
 
-class AForm{
-    protected:
+class Form{
+    private:
         const std::string name;
         bool _signed;
         const int gradeSign;
         const int gradeExec;
 
     public:
-        AForm();
-        AForm(std::string name, int gradeSign, int gradeExec);
-        AForm(const AForm &copy);
-        ~AForm();
-        AForm &operator=(const AForm &op);
+        Form();
+        Form(std::string name, int gradeSign, int gradeExec);
+        Form(const Form &copy);
+        ~Form();
+        Form &operator=(const Form &op);
 
         std::string getName() const;
         bool getSigned() const;
         int getGradeSign() const;
         int getGradeExec() const;
         
-        virtual void beSigned(const Bureaucrat &bureaucrat) = 0;
-        virtual void execute(Bureaucrat const &target) const = 0;
+        void beSigned(const Bureaucrat &bureaucrat);
 
 
         class GradeTooHighException: public std::exception{
@@ -43,6 +41,6 @@ class AForm{
         };
 };
 
-std::ostream& operator<<(std::ostream& os, AForm const& AForm);
+std::ostream& operator<<(std::ostream& os, Form const& form);
 
 #endif
