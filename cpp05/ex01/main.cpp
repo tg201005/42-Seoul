782b1028564ac8ae9d2f2a233e6790b41de6681e
Taekwon Yi <tyi@c6r4s4.42seoul.kr>
2023-06-29 15:17:52 +0900

    change name

--- cpp05/ex01/main.cpp
+++ cpp05/ex01/main.cpp
@@ -1,15 +1,12 @@
 #include "Bureaucrat.hpp"
 
 int main() {
-    //write the code using Form class
-    //write the code using Bureaucrat class
-    //write the code using try/catch
 
-   Bureaucrat b1("b1", 1);
-    Bureaucrat b2("b2", 150);
+   Bureaucrat King("King", 1);
+    Bureaucrat Slave("Slave", 150);
 
-    std::cout << b1 << std::endl;
-    std::cout << b2 << std::endl;
+    std::cout << King << std::endl;
+    std::cout << Slave << std::endl;
 
     Form f1("f1", 1, 1);
     Form f2("f2", 150, 150);
@@ -17,8 +14,8 @@ int main() {
     std::cout << f1 << std::endl;
     std::cout << f2 << std::endl;
 
-    b1.signForm(f1);
-    b1.signForm(f2);
-    b2.signForm(f1);    
-    b2.signForm(f2);
+    King.signForm(f1);
+    King.signForm(f2);
+    Slave.signForm(f1);    
+    Slave.signForm(f2);
 }
