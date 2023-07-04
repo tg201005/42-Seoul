24d871daf6b6293f3a8db6f1edcf94e3b5dd05fc
Taekwon Yi <tyi@c6r4s4.42seoul.kr>
2023-07-04 18:35:18 +0900

    first

--- cpp06/ex00/ScalarConvert.hpp
+++ cpp06/ex00/ScalarConvert.hpp
@@ -1,12 +1,25 @@
 #pragma once
 
 #include <string>
-#include <iostream>
 #include <sstream>
+#include <iostream>
+#include <limits>
+#include <cctype>
 #include <iomanip>
 
-class ScalarConvert
-{
-    private:
+class ScalarConverter {
 
+public:
+    static void convert(const std::string& literal);
+ 
+    
+private:
+    static bool isCharLiteral(const std::string& literal);
+    static void convertChar(const std::string& literal);
+    static bool isIntLiteral(const std::string& literal);
+    static void convertInt(const std::string& literal);
+    static bool isFloatLiteral(const std::string& literal);
+    static void convertFloat(const std::string& literal);
+    static bool isDoubleLiteral(const std::string& literal);
+    static void convertDouble(const std::string& literal);
 };
