3c8c239281ad34e1730e3424fd2118f5b81b82a8
Taekwon Yi <tyi@c6r5s4.42seoul.kr>
2023-07-10 13:14:22 +0900

    implement iter and understanding l and r value

---
+++ cpp07/ex01/iter.hpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+
+template<typename T, typename F>
+void iter(T* array, size_t length, F func) {
+    for(size_t i = 0; i < length; i++) {
+        func(array[i]);
+    }
+}
---
+++ cpp07/ex01/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "iter.hpp"
+
+void printInt(int &x) {
+    std::cout << x << std::endl;
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    std::cout << "-------------------" << std::endl;
+    std::cout << "int array:" << std::endl;
+    iter(arr, 5, printInt);
+
+    std::cout << "-------------------" << std::endl;
+    std::cout << "char array:" << std::endl;
+    char str[] = "Hello";
+    iter(str, 5, printInt);
+
+    // std::cout << "-------------------" << std::endl;
+    // std::cout << "float array:" << std::endl;
+    // float farr[] = {1.1f, 2.2f, 3.3f, 4.4f, 5.5f};
+    // iter(farr, 5, printInt);
+
+
+
+    return 0;
+}
