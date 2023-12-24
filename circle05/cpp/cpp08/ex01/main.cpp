b79640a0fd7c729eba0bd76333ccf86f705bd27c
tg201005 <teemo20001005@gmail.com>
2023-12-24 21:43:18 +0900

    finsih:wq

--- circle05/cpp/cpp08/ex01/main.cpp
+++ circle05/cpp/cpp08/ex01/main.cpp
@@ -5,54 +5,32 @@
 
 
 int main(){
-    Span sp = Span(5);
-    sp.addNumber(6);
-    sp.addNumber(3);
-    sp.addNumber(17);
-    sp.addNumber(9);
-    sp.addNumber(11);
-    std::cout << sp.shortestSpan() << std::endl;
-    std::cout << sp.longestSpan() << std::endl;
-    return 0;
+    try {
+        Span sp = Span(5);
+        sp.addNumber(5);
+        sp.addNumber(3);
+        sp.addNumber(17);
+        sp.addNumber(9);
+        sp.addNumber(11);
+        std::cout << sp.shortestSpan() << std::endl;
+        std::cout << sp.longestSpan() << std::endl;
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+    }
 }
 
-//
-
-// int main() {
-//     Span sp = Span(10001);
-
-//     srand(time(NULL));
-
-//     for (int i = 0; i < 10000; i++) {
-//         sp.addNumber(rand() % 10000);
-//     }
-
-//     sp.print();
-
+// int main (){
 //     try {
+//         Span sp = Span(5);
+//         sp.addNumber(5);
+//         sp.addNumber(3);
+//         sp.addNumber(17);
+//         sp.addNumber(9);
+//         sp.addNumber(11);
 //         std::cout << sp.shortestSpan() << std::endl;
 //         std::cout << sp.longestSpan() << std::endl;
+//         sp.addNumber(11, 20);
 //     } catch (const std::exception& e) {
 //         std::cerr << e.what() << std::endl;
 //     }
-//     return 0;
 // }
-
-// add number 1~5000
-
-//------------------test case 1------------------
-
-// int main() {
-//     Span sp = Span(5000);
-
-//     sp.addNumber(1, 5000);
-
-//     try {
-//         std::cout << sp.shortestSpan() << std::endl;
-//         std::cout << sp.longestSpan() << std::endl;
-//     } catch (const std::exception& e) {
-//         std::cerr << e.what() << std::endl;
-//     }
-//     return 0;
-// }
-
--- circle05/cpp/cpp08/ex02/main.cpp
+++ circle05/cpp/cpp08/ex02/main.cpp
@@ -3,124 +3,52 @@
 #include <list>
 
 
-// int main()
-// {
-//     {        
-//         MutantStack<int> mstack;
-//         mstack.push(5);
-//         mstack.push(17);
+int main()
+{
+        MutantStack<int> mstack;
+        mstack.push(5);
+        mstack.push(17);
 
-//         std::cout << "---------------------" << std::endl;
-//         std::cout << "MutantStack" << std::endl;
-//         std::cout << "---------------------" << std::endl;
+        std::cout << "---------------------" << std::endl;
+        std::cout << "MutantStack" << std::endl;
+        std::cout << "---------------------" << std::endl;
         
-//         std::cout << "Top : " << mstack.top() << std::endl;
+        std::cout << "Top : " << mstack.top() << std::endl;
         
-//         mstack.pop();
-//         std::cout << "Size : " << mstack.size() << std::endl;
-//         mstack.push(3);
-//         mstack.push(5);
-//         mstack.push(737);
-//         //[...]
-//         mstack.push(0);
-//         MutantStack<int>::iterator it = mstack.begin();
-//         MutantStack<int>::iterator ite = mstack.end();
-//         ++it;
-//         --it;
+        mstack.pop();
+        std::cout << "Size : " << mstack.size() << std::endl;
+        mstack.push(3);
+        mstack.push(5);
+        mstack.push(737);
+        //[...]
+        mstack.push(0);
+        MutantStack<int>::iterator it = mstack.begin();
+        MutantStack<int>::iterator ite = mstack.end();
+        ++it;
+        --it;
 
-//         std::cout << "---------------------" << std::endl;
-//         while (it != ite)
-//         {
-//         std::cout << *it << std::endl;
-//         ++it;
-//         }
+        std::cout << "---------------------" << std::endl;
+        while (it != ite)
+        {
+        std::cout << *it << std::endl;
+        ++it;
+        }
 
-//         std::stack<int> s(mstack);
-//     }
-
-
-
-//     {
-//         std::cout << "---------------------" << std::endl;
-//         std::cout << "list" << std::endl;
-//         std::cout << "---------------------" << std::endl;
-        
-//         std::list<int> list;
-//         list.push_back(5);
-//         list.push_back(17);
-        
-//         std::cout << "Top : " << list.back() << std::endl; 
-//         list.pop_back();
-
-//         std::cout << "Size : " << list.size() << std::endl;
-
-//         list.push_back(3);
-//         list.push_back(5);
-//         list.push_back(737);
-//         //[...]
-//         list.push_back(0);
-//         std::list<int>::iterator it = list.begin();
-//         std::list<int>::iterator ite = list.end();
-//         ++it;
-//         --it;
-
-//         std::cout << "---------------------" << std::endl;
-
-//         while (it != ite)
-//         {
-//             std::cout << *it << std::endl;
-//         ++it;
-//         }
-//     }
-
-//     std::cout << "---------------------" << std::endl;
-
-//     return 0;
-// }
-
-
-
-int main(){
-    MutantStack<int> mstack;
-    mstack.push(1);
-    mstack.push(2);
-    mstack.push(3);
-    mstack.push(4);
-    mstack.push(5);
-    mstack.push(6);
-    mstack.push(7);
-    mstack.push(8);
-    mstack.push(9);
-
-    std::cout << "---------------------" << std::endl;
-    std::cout << "MutantStack" << std::endl;
-    std::cout << "---------------------" << std::endl;
+        std::stack<int> s(mstack);
     
-    std::cout << "Top : " << mstack.top() << std::endl;
+
+        std::cout << "---------------------" << std::endl; 
+        
+
+        MutantStack<int> mstack2(mstack);
+        std::cout << "MutantStack2" << std::endl;
+        mstack2.print();
+        std::cout << "---------------------" << std::endl;
+
     
-    mstack.pop();
-    mstack.pop();
-    mstack.pop();
-    std::cout << "Size : " << mstack.size() << std::endl;
-    mstack.push(3);
-    mstack.push(5);
-    mstack.push(737);
-    //[...]
-    mstack.push(0);
-    MutantStack<int>::iterator it = mstack.begin();
-    MutantStack<int>::iterator ite = mstack.end();
-
-    std::cout << "---------------------" << std::endl;
 
 
     std::cout << "---------------------" << std::endl;
-    std::cout << "MutantStack" << std::endl;
-    mstack.print();
-    std::cout << "---------------------" << std::endl;
-    
-    MutantStack<int> mstack2(mstack);
-    std::cout << "MutantStack2" << std::endl;
-    mstack2.print();
-    std::cout << "---------------------" << std::endl;
 
+    return 0;
 }
